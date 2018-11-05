17fbd5b9fe748238d6ab45d28b7413db3155132a
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-05 22:25:35 +0530

    Complete Procedure and Policies Page

---
+++ src/components/HR.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const hr = () => <hr className="custom-hr" />;
+
+export default hr;
--- src/components/Procedure.js
+++ src/components/Procedure.js
@@ -1,6 +1,143 @@
-import React from 'react'
+import React from "react";
+import HR from "./HR";
 
-const Procedure=props=>(<div>Home</div>);
+const Procedure = props => (
+  <div className="procedure-outer">
+    <div className="procedure-wrapper">
+      <h1>Procedures and Policies</h1>
+      <h2>Job Policy</h2>
+      <p>
+        Companies visit campus and select students as per their recruitment
+        drive.
+      </p>
+      <p>
+        To a student finally selected by a company after all testing and
+        interview rounds, the company may offer :
+      </p>
+      <h2>Full time job offer</h2>
+      <p>
+        The selected student will join company in June 2019 as full time
+        employee.
+      </p>
+      <h2>Internship</h2>
+      <p>
+        B. Tech 3rd year students are eligible for <strong>2 months</strong>{" "}
+        summer internship The student will join the company in May/June 2019 as
+        an intern for a period of 2 months.
+      </p>
+      <p>
+        B. Tech. 4th year students are eligible for <strong>6 months</strong>{" "}
+        internship. The student will join the company in January 2019 as an
+        intern for a period of 6 months and complete their 8th semester project
+        in the company.
+      </p>
+      <HR />
+      <h2>Placement Procedure</h2>
+      <ul className="custom-list">
+        <li>
+          The Training and Placement Office, IIITL sends invitations alongwith
+          Job Notification Form(JNF) to organizations to participate in the
+          campus placements.
+        </li>
+        <li>
+          Organization fills in the JNF containing details of the job offer (pay
+          package, place of posting, allowances and other bonuses). JNF can be
+          sent by email to Training and Placement Office, IIITL (
+          <a href="mailto:placements@iiitl.ac.in">placements@iiitl.ac.in</a>
+          ).
+        </li>
+        <li>
+          The organization details alongwith job offer is made available to the
+          students on a private group, requesting them to apply.
+        </li>
+        <li>
+          Training and Placement Office, IIITL allots date(s) to an organization
+          for campus placements. The organization confirms the date(s) to
+          Training and Placement Office, IIITL.
+        </li>
+        <li>
+          The list of the students who have applied is provided to the
+          organization along with their Resumes.
+        </li>
+        <li>
+          Organizations come to campus on the allotted date(s) and conduct tests
+          and/or interviews as per their recruitment process.
+        </li>
+        <li>
+          The organization is expected to furnish the list of selected
+          candidates at the end of assigned interview slot, preferably on the
+          day of campus visit.
+        </li>
+        <li>
+          Training and Placement Office, IIITL also coordinates the signing of
+          offer letter by students who have been selected to ensure that they
+          reach the organization/company asap.
+        </li>
+      </ul>
+      <HR />
+      <h2 className="policies-heading">Placement Policy</h2>
+      <h2>Classification of Company</h2>
+      <p>
+        The classification of a company will based on the criteria of pay
+        packages(CTC) and work profile. The two classes being:
+      </p>
+      <ul className="custom-list">
+        <li>First Round Company</li>
+        <li>Dream Round Company</li>
+      </ul>
+      <p>
+        Overall CTC of a company includes joining bonus and all other perks, if
+        applicable. Eligibility for a dream round company for a student will be
+        decided on the basis of overall CTC.
+      </p>
+      <h2>Job Offer for a student</h2>
+      <ul className="custom-list">
+        <li>
+          If a student's name appears on the final shortlist declared after the
+          Company's process through the Placement Cell, then that would be
+          considered as a Job Offer to the student.
+        </li>
+        <li>
+          A student will be out of campus placement process only if he/she gets
+          an offer from a Dream Round company.
+        </li>
+        <li>
+          Students getting a Job Offer from a First Round company are still
+          eligible for further placements and only these students are allowed to
+          upgrade to a Dream Round company.
+        </li>
+        <li>
+          PPO Offer and off-campus placements are also considered as Job Offer,
+          hence the same policy is applicable to them.
+        </li>
+        <h2>Job Offer made by Company</h2>
+        <ul className="custom-list">
+          <li>
+            The companies are requested to announce the result/ final shortlist
+            on the day of campus visit.
+          </li>
+          <li>
+            The company should hand over the duly signed hard copy of the final
+            selection list to the placement cell.
+          </li>
+          <li>
+            The company shall provide the offer letters to the Placement office
+            and not directly to the students.
+          </li>
+          <li>
+            The purview of the Placement Cell is restricted only to the offers
+            made as part of the campus placement process.
+          </li>
+          <li>
+            In case the company is unable to declare the result on the same day,
+            then the student is allowed to participate in other companies until
+            the result is declared. The final status will depend upon who
+            declares the result first.
+          </li>{" "}
+        </ul>
+      </ul>
+    </div>
+  </div>
+);
 
-
-export default Procedure
+export default Procedure;
