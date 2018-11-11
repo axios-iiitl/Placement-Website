008151a9c2cffb164c5af6ce0925dbb0c00f89bb
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-11 21:16:49 +0530

    Edit Contact Us Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,88 +7,72 @@ const contactUs = () => {
   return (
     <React.Fragment>
       <div className="contact-outer">
-        <div className="contact-wrapper">
-          <div className="contact-box">
-            <h1>Contact Info</h1>
-            <div className="contact-info">
-              <p>
-                <strong>Email:</strong>{" "}
-                <a href="mailti:contact@iiitl.ac.in">contact@iiitl.ac.in</a>
-              </p>
-              <p>
-                <strong>Phone:</strong>{" "}
-                <a href="tel:+0532 292 2032">+0532 292 2032</a>
-              </p>
-              <p>
-                <strong>Address:</strong> IIIT Allahabad, Jhalwa, Allahabad -
-                211015
-              </p>
-            </div>
-          </div>
-        </div>
+        <div className="contact-wrapper" />
         <div className="placement-team">
           <h1>Placement Cell</h1>
           <Card
-            src="https://randomuser.me/api/portraits/men/51.jpg"
+            src="https://iiitl.ac.in/sites/default/files/inline-images/vijayshri_0_0.jpg"
             alt="xyz"
-            name="NAME XYZABC"
+            name="Dr. Vijayshri Tewari"
             isTeacher={false}
-            position="Placement Officer"
-            email="xyz@abcd.com"
-            phone="+91-987654321"
+            position="Training and Placements Officer"
+            email="tpo@iiitl.ac.in"
+            phone="+91-9415214707"
           />
           <h2>Student Coordinators</h2>
           <div className="placement-coordinators">
             <div className="placement-coordinators-wrapper">
               <Card
-                src="https://randomuser.me/api/portraits/men/51.jpg"
+                src="https://iiitl.ac.in/sites/default/files/inline-images/devanshoo_1.jpg"
                 alt="xyz"
-                name="NAME XYZABC"
+                name="Devanshoo Udhani"
                 isTeacher={false}
-                position="Placement Officer"
-                email="xyz@abcd.com"
-                phone="+91-987654321"
+                position="Student Coordinator"
+                email="placements@iiitl.ac.in"
+                phone="+91-9838 636124"
               />
               <Card
-                src="https://randomuser.me/api/portraits/men/51.jpg"
+                src="https://iiitl.ac.in/sites/default/files/inline-images/prankur_0.jpg"
                 alt="xyz"
-                name="NAME XYZABC"
+                name="Prankur Agarwal"
                 isTeacher={false}
-                position="Placement Officer"
-                email="xyz@abcd.com"
-                phone="+91-987654321"
+                position="Student Coordinator"
+                email="placements@iiitl.ac.in"
+                phone="+91-9457 382069"
               />
             </div>
           </div>
           <h2>Executives</h2>
-          <div className="placement-executives">
-            <Card
-              src="https://randomuser.me/api/portraits/men/51.jpg"
-              alt="xyz"
-              name="NAME XYZABC"
-              isTeacher={false}
-              position="Placement Officer"
-              email="xyz@abcd.com"
-              phone="+91-987654321"
-            />
-            <Card
-              src="https://randomuser.me/api/portraits/men/51.jpg"
-              alt="xyz"
-              name="NAME XYZABC"
-              isTeacher={false}
-              position="Placement Officer"
-              email="xyz@abcd.com"
-              phone="+91-987654321"
-            />
-            <Card
-              src="https://randomuser.me/api/portraits/men/51.jpg"
-              alt="xyz"
-              name="NAME XYZABC"
-              isTeacher={false}
-              position="Placement Officer"
-              email="xyz@abcd.com"
-              phone="+91-987654321"
-            />
+          <div className="placement-executives-wrapper">
+            <div className="placement-executives">
+              <Card
+                src="https://iiitl.ac.in/sites/default/files/inline-images/shubh_0.jpg"
+                alt="xyz"
+                name="Shubhanshu Singh"
+                isTeacher={false}
+                position="Placement Executive"
+                email="xyz@abcd.com"
+                phone="+91-9455229539"
+              />
+              <Card
+                src="https://iiitl.ac.in/sites/default/files/inline-images/vaibhav_0.jpg"
+                alt="xyz"
+                name="Vaibhav Agarwal"
+                isTeacher={false}
+                position="Placement Officer"
+                email="placements@iiitl.ac.in"
+                phone="+91-7839932719"
+              />
+              <Card
+                src="https://iiitl.ac.in/sites/default/files/inline-images/IMG-20180405-WA0001.jpg"
+                alt="xyz"
+                name="Raghav Khandelwal"
+                isTeacher={false}
+                position="Placement Officer"
+                email="placements@iiitl.ac.in"
+                phone="+91-9414038742"
+              />
+            </div>
           </div>
         </div>
       </div>
