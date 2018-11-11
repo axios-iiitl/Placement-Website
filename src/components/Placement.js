ecc99256043c9dae6b87ac3899ae9e7355e18dcd
Divy123 <lit2017001@iiitl.ac.in>
2018-11-11 16:24:40 +0530

    Added stats page

--- src/components/Placement.js
+++ src/components/Placement.js
@@ -3,43 +3,40 @@ import PlacementStatsCard from "./PlacementStatsCard";
 
 const Placement = props => {
   return (
-    <div className="place-stats-outer-wrapper">
-      <div className="place-stats-main-heading">
-        <h1>Placement Statistics (2018)</h1>
-      </div>
-      <ul className="palcement-stats-ul">
-        <PlacementStatsCard
-          cardText="Average Salary"
-          cardStat="11.08"
-          cardUnit="LPA"
-        />
-        <PlacementStatsCard
-          cardText="Average Of Top 20%"
-          cardStat="20.50"
-          cardUnit="LPA"
-        />
-        <PlacementStatsCard
-          cardText="Average Of Top 50%"
-          cardStat="12.83"
-          cardUnit="LPA"
-        />
-        <PlacementStatsCard
-          cardText="Highest Package"
-          cardStat="27.00"
-          cardUnit="LPA"
-        />
-        <PlacementStatsCard
-          cardText="Lowest Package"
-          cardStat="4.50"
-          cardUnit="LPA"
-        />
-        <PlacementStatsCard
-          cardText="Placed Percentage"
-          cardStat="67.50"
-          cardUnit="Percentage"
-        />
-      </ul>
+    <div className='stats'>
+    <h1>Placement and Statistics</h1>
+     <p>
+     IIIT-Lucknow provides a platform to facilitate interaction between students and companies, so both can find the best match as per their aspirations and requirements.<br /><br />
+
+Our Graduates are a combination of rigorous thinking, hard work and fundamentals. The campus promotes a host of student activities to improve their soft skills, which are imperative for one to excel in his/ her work space. Besides, campus is abuzz with activities of various student clubs and organizations.<br /><br />
+
+We highly value our partnership with recruiters & remain committed to making your recruiting experience productive and positive. On behalf of the campus the placement team extends a warm welcome to all the recruiters to visit the campus and test our budding talents. We hope that firms and our students will create synergies & find the best match between their needs and capabilities.<br /><br />
+
+Here are some Statistics regarding the placement of the batch graduating in 2019:<br /><br />
+<ul>
+  <li>
+    Highest Package: 27 lpa
+  </li>
+  <li>
+  Lowest Package: 4.50 lpa
+  </li>
+  <li>
+    Average of top 50%: 12.83 lpa
+  </li>
+  <li>
+  Average of top 20%: 20.50 lpa
+  </li>
+  <li>
+    Average of total students: 11.08 lpa
+  </li>
+  <li>
+    Number of students registered: 40
+  </li>
+</ul>
+     </p>
     </div>
+  
+   
   );
 };
 
