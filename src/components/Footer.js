7a5f2bf1a43e09b6f006a5f502b9105bd09f6801
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-14 14:45:27 +0530

    Change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,88 +2,66 @@ import React from "react";
 import TwitterSVG from "./svg/Twitter";
 import FacebookSVG from "./svg/Facebook";
 import LinkedInSVG from "./svg/Linkedin";
-import iiitlText from "../assets/images/iiitl-text.svg"
+import iiitlText from "../assets/images/iiitl-text.svg";
 
 const footer = () => {
   return (
-    <footer>
-      <div className="centered clearfix">
-        <div className="footer-logo">
-          <img className="logo" src={iiitlText} alt="Logo" />
-          <div className="social">
-            <a href="/" className="facebook">
-              <FacebookSVG />
-            </a>
-            <a href="/" className="twitter">
-              <TwitterSVG />
-            </a>
-            <a href="/" className="linkedin">
-              <LinkedInSVG />
-            </a>
-          </div>
-        </div>
-        <div className="footer-contact">
-          <h3>
-            Contact Us:
-          </h3>
-          <p>
-            <a href="mailto:contact@iiitl.ac.in">contact@iiitl.ac.in</a>
-          </p>
-          <p>
-            <a href="tel:+0532 292 2032">+0532 292 2032</a>
-          </p>
-          <p>
-              IIIT Allahabad, Jhalwa
-              <br />
-              Allahabad - 211015
-          </p>
-        </div>
-        <div className="footer-navigation">
-          <div className="footer-links-holder">
-            <ul className="footer-links">
-              <li>
-                <a href="/">Why IIITL?</a>
-              </li>
-              <li>
-                <a href="/">Facilities</a>
-              </li>
-              <li>
-                <a href="/">Academics</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-links-holder">
-            <ul className="footer-links">
-              <li>
-                <a href="/">Procedures and<br/>Policies</a>
-              </li>
-              <li>
-                <a href="/">Brochure</a>
-              </li>
-              <li>
-                <a href="/">Past Recruiters</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-links-holder">
-            <ul className="footer-links">
-              <li>
-                <a href="/">Placement Statistics</a>
-              </li>
-              <li>
-                <a href="/">The Team</a>
-              </li>
-              <li>
-                <a href="/">Contact Us</a>
-              </li>
-            </ul>
+    <React.Fragment>
+      <div class="footer">
+        <div class="footer-outer-wrapper">
+          <div class="footer-inner-wrapper">
+            <div class="footer-col">
+              <h2>Contact Us</h2>
+              <p>
+                <a href="mailto:contact@iiitl.ac.in">contact@iiitl.ac.in</a>
+              </p>
+              <p>
+                <a href="tel:+0532 292 2032">+0532 292 2032</a>
+              </p>
+              <p>
+                IIIT Allahabad, Jhalwa,
+                <br />
+                Allahabad - 211015
+              </p>
+            </div>
+            <div class="footer-col">
+              <h2>Coordinators</h2>
+              <p className="footer-contact-name">Devanshoo Udhani</p>
+              <p className="footer-contact-position">Student Coordinator</p>
+              <p>+91-9415214707</p>
+              <p>placements@iiitl.ac.in</p>
+            </div>
+            <div class="footer-col">
+              <p className="footer-contact-name">Prankur Agarwal</p>
+              <p className="footer-contact-position">Student Coordinator</p>
+              <p>+91-9415214707</p>
+              <p>placements@iiitl.ac.in</p>
+            </div>
+            <div class="footer-col">
+              <h2>Links</h2>
+              <p>
+                <a href="#">Brochure</a>
+              </p>
+              <p>
+                <a href="#">Academics</a>
+              </p>
+              <p>
+                <a href="#">Facilities</a>
+              </p>
+              <p>
+                <a href="#">The Team</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="bottom-bar">
-        All Rights Reserved © 2018
+
+      <div class="footer-copyright">
+        <p>
+          ALL RIGHTS RESERVED © 2018. <strong>IIIT LUCKNOW</strong>
+        </p>
       </div>
-    </footer>
+    </React.Fragment>
   );
 };
 
