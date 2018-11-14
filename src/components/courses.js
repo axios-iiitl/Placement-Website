111b16cb5030a0f678822c52b89d2b787678e808
Divy123 <lit2017001@iiitl.ac.in>
2018-11-14 22:12:55 +0530

    stats page changed

---
+++ src/components/courses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Courses=()=>(
+    <div>
+     <h2>Undegraduate Courses</h2>
+     <ul>
+      <li>Information Technology</li>   
+     </ul>
+    </div>
+)
+
+export default Courses;
