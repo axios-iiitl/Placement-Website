7dda3148687a89eb42888aa5a7897e014f05ba5d
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-15 00:19:14 +0530

    Change statistics page

--- src/components/Placement.js
+++ src/components/Placement.js
@@ -2,44 +2,65 @@ import React from "react";
 
 const Placement = props => {
   return (
-    <div className="stats">
-      <h1>Statistics</h1>
-      <p>
-        IIIT-Lucknow provides a platform to facilitate interaction between
-        students and companies, so both can find the best match as per their
-        aspirations and requirements.
-        <br />
-        <br />
-        Our Graduates are a combination of rigorous thinking, hard work and
-        fundamentals. The campus promotes a host of student activities to
-        improve their soft skills, which are imperative for one to excel in his/
-        her work space. Besides, campus is abuzz with activities of various
-        student clubs and organizations.
-        <br />
-        <br />
-        We highly value our partnership with recruiters & remain committed to
-        making your recruiting experience productive and positive. On behalf of
-        the campus the placement team extends a warm welcome to all the
-        recruiters to visit the campus and test our budding talents. We hope
-        that firms and our students will create synergies & find the best match
-        between their needs and capabilities.
-        <br />
-        <br />
-        Here are some Statistics regarding the placement of the batch graduating
-        in 2019:
-        <br />
-        <br />
-        </p>
-        <ul style={{marginLeft: "2vw"}}>
-          <li>Highest Package: 27 lpa</li>
-          <li>Lowest Package: 4.50 lpa</li>
-          <li>Percentage of students placed: 67.50%</li>
-          <li>Average of top 50%: 12.83 lpa</li>
-          <li>Average of top 20%: 20.50 lpa</li>
-          <li>Average of total students: 11.08 lpa</li>
-          <li>Number of students registered: 40</li>
-        </ul>
-    </div>
+    <React.Fragment>
+      <div className="stats-outer">
+        <div className="stats-inner">
+          <h1>Statistics</h1>
+          <p>
+            IIIT-Lucknow provides a platform to facilitate interaction between
+            students and companies, so both can find the best match as per their
+            aspirations and requirements.
+          </p>
+          <p>
+            Our Graduates are a combination of rigorous thinking, hard work and
+            fundamentals. The campus promotes a host of student activities to
+            improve their soft skills, which are imperative for one to excel in
+            his/ her work space. Besides, campus is abuzz with activities of
+            various student clubs and organizations.
+          </p>
+          <p>
+            We highly value our partnership with recruiters & remain committed
+            to making your recruiting experience productive and positive. On
+            behalf of the campus the placement team extends a warm welcome to
+            all the recruiters to visit the campus and test our budding talents.
+            We hope that firms and our students will create synergies & find the
+            best match between their needs and capabilities.
+          </p>
+        </div>
+      </div>
+      <div class="stats-box-outer-wrapper">
+        <div class="stats-box-inner-wrapper">
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">27 LPA</p>
+            <p class="stats-box-heading">Highest Package</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">4.5 LPA</p>
+            <p class="stats-box-heading">Lowest Package</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">67.50%</p>
+            <p class="stats-box-heading">Percentage of students placed</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">12.83 LPA</p>
+            <p class="stats-box-heading">Average of top 50%</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">20.50 LPA</p>
+            <p class="stats-box-heading">Average of top 20%</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">11.08 LPA</p>
+            <p class="stats-box-heading">Average of total students</p>
+          </div>
+          <div class="stats-box-outer">
+            <p class="stats-box-numbers">40</p>
+            <p class="stats-box-heading">Number of students registered</p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
