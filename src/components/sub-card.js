dbf457c866f15050d87a19440b5c48507abbad1f
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-15 02:16:07 +0530

    Improve responsiveness

--- src/components/sub-card.js
+++ src/components/sub-card.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import {subjects} from './semlist'
-const SubCard=props=>(
-    <div>
-<ul className="cards">
-  <li className="cards__item">
-    <div className="card">
-      <div className="card__content">
-        <div className="card__title"><p className="sem">{props.sem}</p></div>
-        <ul className='sub'>
-          {subjects[props.ind].map(val=><li>{val}</li>)}
-        </ul>
+import React from "react";
+import { subjects } from "./semlist";
+const SubCard = props => (
+  <ul className="cards">
+    <li className="cards__item">
+      <div className="card">
+        <div className="card__content">
+          <div className="card__title">
+            <p className="sem">{props.sem}</p>
+          </div>
+          <ul className="sub">
+            {subjects[props.ind].map(val => (
+              <li>{val}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </li>
-</ul>
-    </div>
-)
+    </li>
+  </ul>
+);
 
 export default SubCard;
