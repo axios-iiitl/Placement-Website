2b8be16e00e64f3682a8212bb83866e5d9dd6605
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-16 13:52:35 +0530

    Change Past to Our

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -26,7 +26,7 @@ const Toolbar = props => (
                             <NavLink to="/demographics" exact className="activeclass">Demographics</NavLink>
                         </div>
                     </li>
-                    <li><NavLink to="/past" exact className="activeclass">Past Recruiters</NavLink></li>
+                    <li><NavLink to="/past" exact className="activeclass">Our Recruiters</NavLink></li>
                     <li><NavLink to="/statistics" exact className="activeclass">Statistics</NavLink></li>
                     <li><NavLink to="/why" exact className="activeclass">Why Us</NavLink></li>
                     <li><NavLink to="/procedure" exact className="activeclass">Procedure</NavLink></li>
