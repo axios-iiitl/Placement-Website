dbf81025a915a4ffd7ac5ccba5e46914812e7eb8
divy123 <lit2017001@iiitl.ac.in>
2018-11-21 03:37:57 +0530

    ch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -71,8 +71,7 @@ const footer = () => {
         <p>
           ALL RIGHTS RESERVED © 2018. <strong>IIIT LUCKNOW</strong> (Developed &
           Managed By -{" "}
-          <a href="https://www.linkedin.com/in/divy-khare-63a398172/" target="none">Divy</a>,{" "}
-          <a href="https://www.linkedin.com/in/vaibhavgupta79/" target="none">Vaibhav</a>)
+          <a href="https://www.linkedin.com/in/vaibhavgupta79/" target="none">Vaibhav Gupta</a>)
         </p>
       </div>
     </footer>
