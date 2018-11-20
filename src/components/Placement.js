8ffca8bcf61143930d6be5690043e1447d2c7a44
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-20 17:08:02 +0530

    Prettier

--- src/components/Placement.js
+++ src/components/Placement.js
@@ -1,8 +1,5 @@
 import React from "react";
-import PlacementStats2018 from "../assets/images/placement-stats-2018.png";
-import BarChart1 from "../assets/images/bar-chart-1.jpg";
-import BarChart2 from "../assets/images/bar-chart-2.png";
-import avg from "../assets/images/avg.png";
+
 const Placement = props => {
   return (
     <div className="stats">
@@ -15,18 +12,17 @@ const Placement = props => {
       <img className="head" src="https://i.imgur.com/XRYTi13.png" />
       <br />
       <span className="txt">
-        Our Graduates are a combination of rigorous thinking, hard work and
+        <p>Our Graduates are a combination of rigorous thinking, hard work and
         fundamentals. The campus promotes a host of student activities to
         improve their soft skills, which are imperative for one to excel in his/
         her work space. Besides, campus is abuzz with activities of various
-        student clubs and organizations.
-        <br />
-        We highly value our partnership with recruiters & remain committed to
+        student clubs and organizations.</p>
+        <p>We highly value our partnership with recruiters & remain committed to
         making your recruiting experience productive and positive. On behalf of
         the campus the placement team extends a warm welcome to all the
         recruiters to visit the campus and test our budding talents. We hope
         that firms and our students will create synergies & find the best match
-        between their needs and capabilities.{" "}
+        between their needs and capabilities.</p>
       </span>
       <img className="avg" src="https://i.imgur.com/wk9DGoJ.png" />
       <div style={{ float: "left", marginLeft: "20px" }}>
