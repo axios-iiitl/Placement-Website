f0482228808e012b490b5488e6047030b4c96f90
dojutsu-user <vaibhgupt199@gmail.com>
2018-11-21 19:42:58 +0530

    improvements in about page

--- src/components/about.js
+++ src/components/about.js
@@ -15,9 +15,9 @@ const About = props => (
           w.e.f. the academic session 2015-16.
         </p>
         <p>
-          The institute currently operates from IIIT Allahabad(the mentor
-          institute) in Deoghat,Jhalwa Allahabad.It currently offers all the
-          amenities ,academic and non-academic to its students that can help
+          The institute currently operates from IIIT Allahabad (the mentor
+          institute) in Deoghat, Jhalwa, Allahabad (Prayagraj). It currently offers all the
+          amenities, academic and non-academic to its students that can help
           them flourish and serve the nation with all their apprehension in the
           various fields of technology.
         </p>
@@ -27,7 +27,7 @@ const About = props => (
         </p>
         <p>
           The permanent campus of IIIT Lucknow is coming up in 50 acres of land
-          at Chak Ganjaria Region,Gomti Nagar Extension, Lucknow. The Institute
+          at Chak Ganjaria Region, Gomti Nagar Extension, Lucknow. The Institute
           is being set up with the financial contributions of MHRD, Govt. of
           India, Govt. of Uttar Pradesh, and U. P. Electronics Corporation Ltd.
           as industry partners.
@@ -36,7 +36,7 @@ const About = props => (
           The mission of IIIT Lucknow is to be a unique and world class
           nucleating "Apex Center of Excellence" in the area of Information
           Technology so as to enhance India's Technological strength in
-          Information Technology . IIIT seeks to derive its strength from a
+          Information Technology. IIIT seeks to derive its strength from a
           linkage with sound Indian traditions of centuries past, and sets out
           to create knowledge-based resources in regional languages.
         </p>
