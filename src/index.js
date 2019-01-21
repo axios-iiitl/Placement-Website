93f4eeb9742060ccd42c9ea4031cc7b8a1ced4ad
dojutsu-user <vaibhgupt199@gmail.com>
2019-01-21 18:00:37 +0530

    use hashrouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-    , document.getElementById('root'));
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
