b7ab004c25af02e8a88e2bca383c3f7249c88b2a
dojutsu-user <vaibhgupt199@gmail.com>
2019-03-26 14:17:39 +0530

    Update

--- src/components/Placement.js
+++ src/components/Placement.js
@@ -11,7 +11,7 @@ const Placement = props => {
   return (
     <div className="stats-outer-wrapper">
       <h1 className="ps-name-heading, pr-main-heading">Placement Statistics 2018-19</h1>
-      <p style={{float:'right',fontSize:'15px'}}>* till January 2019</p>
+      <p style={{float:'right',fontSize:'15px'}}>* till March 2019</p>
       <div className="images-row-2">
         <div className="images-row-2-inner">
           <img src={ArrowsImg} alt="Placement Stats 2018" />
