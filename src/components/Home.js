40df342af8abeba3f61c9565622a64846fa54517
dojutsu-user <vaibhgupt199@gmail.com>
2019-04-19 11:06:49 +0530

    Update details and pics

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import batch2015 from "../assets/images/batch2015.jpg";
+import iiitl from "../assets/images/iiitl.jpeg";
 
 import Ex from "./ex";
 const Home = props => (
   <div>
     <div className="homepic">
-      <img src={batch2015} alt="" className="logo-img" />
+      <img src={iiitl} alt="" className="logo-img" />
     </div>
     <h1 className="elegantshadow">The Institute Speaks</h1>
     <Ex />
