202bc380946f69fa3f4d6f201d012b8dde8073f9
bajajvinamr <lit2019023@iiitl.ac.in>
2021-05-15 17:58:30 +0530

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -75,14 +75,18 @@ const footer = () => {
           ALL RIGHTS RESERVED © 2021. <strong>IIIT LUCKNOW.</strong> (Developed
           & Managed By -{" "}
           <a
-            href="https://www.linkedin.com/in/divy-khare-63a398172/"
+            href="https://github.com/narendraiiitl"
             target="none"
           >
-            Divy
+            Narendra
           </a>
-          ,{" "}
-          <a href="https://www.linkedin.com/in/vaibhavgupta79/" target="none">
-            Vaibhav
+          ,{"  "}
+          <a href="https://www.linkedin.com/in/blurry-x-face/" target="none">
+            Rishabh
+          </a>
+           , {" "}
+          <a href="https://www.linkedin.com/in/vinamrbajaj/" target="none">
+            Vinamr 
           </a>
           )
         </p>
