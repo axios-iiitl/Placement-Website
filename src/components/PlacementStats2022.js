c0ae567f2c86c2d6906c8984c34ec92b19b4e3a7
K Tejeshwar Reddy <lci2020055@iiitl.ac.in>
2022-02-23 22:24:13 +0530

    added 2022 stats

---
+++ src/components/PlacementStats2022.js
@@ -0,0 +1,38 @@
+import React from "react";
+import GearsImg from "../assets/images/gear20.png";
+import PlacementStatsDomainWise from "./PlacemetsStatsDomainWise2021";
+import PlacementStats from "../assets/images/stats20.png";
+import PlacedPercentageImage from "../assets/images/placedpercentage.PNG";
+
+const Placement = props => {
+    
+
+    return (
+        <div className="stats-outer-wrapper" style={{ marginBottom: "30px" }}>
+            <h1 className="ps-name-heading pr-main-heading">
+                Placement Statistics (B.Tech) 2021-21\2
+      </h1>
+            <div className="images-row-2">
+                <div className="images-row-2-inner">
+                    <img src={PlacementStats} alt="Placement Stats 2020" />
+                    <img src={GearsImg} alt="Placement Stats 2020" />
+                </div>
+            </div>
+            <div className="images-row-2" style={{ marginTop: "30px" }}>
+                <div className="images-row-2-inner" style={{ marginBottom: 100 }}>
+                    <PlacementStatsDomainWise />
+                    <div style={{ alignSelf: "center" }}>
+                        <img
+                            src={PlacedPercentageImage}
+                            alt="Placed Percentage"
+                            style={{ width: "250px" }}
+                        />
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Placement;
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -43,6 +43,9 @@ const Toolbar = props => (
               <i className="fa fa-caret-down" />
             </button>
             <div className="dropdown-content">
+            <NavLink to="/placement-stats-2022" exact className="activeclass">
+                Placement Stats 2022
+              </NavLink>
               <NavLink to="/placement-stats-2021" exact className="activeclass">
                 Placement Stats 2021
               </NavLink>
